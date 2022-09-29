c952a6839e44ba8c7a8807fae77a3a9d327094c4
Marioni111 <45676523+Marioni111@users.noreply.github.com>
2022-09-29 13:53:15 -0500

    Add files via upload
    se agregaron todos los archivos

---
+++ vistas/JQuery/JQuery1/Ejemplo1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#ej1").click(function(){
+        $("#div1").hide(1000);
+    });
+    $("#ej2").click(function(){
+        $("#div1").show(1000);
+    });
+    $("#ej3").click(function(){
+        $("#div1").fadeOut(2000);
+    });
+    $("#ej4").click(function(){
+        $("#div1").fadeIn(2000);
+    });
+});
